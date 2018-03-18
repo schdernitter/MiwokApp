c1381727ad6f2dd94e57beed414842dbd4045881
schdernitter <schdernitter@gmail.com>
2018-03-18 15:22:44 +0100

    Added activities

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,13 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +31,66 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+
+        //Find view that shows numbers category and set onclicklistener
+        TextView numbers = findViewById(R.id.numbers);
+        TextView family_members = findViewById(R.id.family);
+        TextView colors = findViewById(R.id.colors);
+        TextView phrases = findViewById(R.id.phrases);
+
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(myintent);
+
+            }
+        });
+        family_members.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(MainActivity.this, FamilyMembersActivity.class);
+                startActivity(myintent);
+
+            }
+        });
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(myintent);
+
+            }
+        });
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(myintent);
+
+            }
+        });
     }
+
+    public void openNumbersActivity(View view) {
+        Intent myintent = new Intent(MainActivity.this, NumbersActivity.class);
+        startActivity(myintent);
+    }
+
+    public void openFamilyMembersActivity(View view) {
+        Intent myintent = new Intent(MainActivity.this, FamilyMembersActivity.class);
+        startActivity(myintent);
+    }
+
+    public void openColorsActivity(View view) {
+        Intent myintent = new Intent(MainActivity.this, ColorsActivity.class);
+        startActivity(myintent);
+    }
+
+    public void openPhrasesActivity(View view) {
+        Intent myintent = new Intent(MainActivity.this, PhrasesActivity.class);
+        startActivity(myintent);
+    }
+
 }
